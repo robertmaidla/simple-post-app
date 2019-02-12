b07a3dae38d65d7bca93219609f3823243b3770e
Robert Maidla <robertmaidla@gmail.com>
2019-02-12 14:53:09 +0200

    Initial backend created

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -10,8 +10,21 @@ router.get('/', async (req, res) => {
 });
 
 // Add Posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
 
 // Delete Posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
 
 async function loadPostsCollection() {
     const client = await mongodb.MongoClient.connect
